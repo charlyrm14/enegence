864ba956a3e4c08feb48ceb59f014caa62f7eb44
charlyrm14 <charlyrm14@gmail.com>
2025-09-19 00:17:51 -0600

    feature(): Se agrega modelo, controlador y migracion para tabla estados

---
+++ app/Models/State.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class State extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'slug',
+        'visibility'
+    ];
+}
