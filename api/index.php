f4c1630f274f1082193af145525da7ab4fb4858f
Carlos Ramos <105004784+charlyrm14@users.noreply.github.com>
2025-09-29 14:05:53 -0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,3 @@
 <?php
-/**
- * Este archivo actúa como el punto de entrada (entry point)
- * de nuestra función serverless en Vercel.
- * Redirige la ejecución al archivo public/index.php de Laravel.
- */
+
 require __DIR__ . '/../public/index.php';
